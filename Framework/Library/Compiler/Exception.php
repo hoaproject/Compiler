5dddf659f18f079c5f5df18a82b767b48e5b2c49
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-02-07 11:59:33 +0100

    Change Hoa_Exception for Hoa_Core_Exception.

--- Framework/Library/Compiler/Exception.php
+++ Framework/Library/Compiler/Exception.php
@@ -35,7 +35,7 @@
 /**
  * Class Hoa_Compiler_Exception.
  *
- * Extending the Hoa_Exception class.
+ * Extending the Hoa_Core_Exception class.
  *
  * @author      Ivan ENDERLIN <ivan.enderlin@hoa-project.net>
  * @copyright   Copyright (c) 2007, 2010 Ivan ENDERLIN.
@@ -46,4 +46,4 @@
  * @subpackage  Hoa_Compiler_Exception
  */
 
-class Hoa_Compiler_Exception extends Hoa_Exception { }
+class Hoa_Compiler_Exception extends Hoa_Core_Exception { }

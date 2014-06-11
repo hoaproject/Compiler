a3f701d58edd2c5688b4b7e4142c488893ec6353
flip111 <flip101@gmail.com>
2014-06-11 21:18:44 +0200

    Tabs to spaces on unit test

--- tests/units/Llk.php
+++ tests/units/Llk.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hoa\Compiler\tests\units;
 
 use Hoa\Compiler\Llk\Llk as LlkTest;
@@ -6,66 +7,68 @@ use Hoa\File\Read;
 use mageekguy\atoum;
 
 class Llk extends atoum\test {
-	private function pathsFromFile($pp) {
-		$compiler = LlkTest::load(new Read($pp));
 
-		$paths = $compiler->getRulePathsFromRoot();
-		$pathStrings = $compiler->getTokenPaths($paths);
+    private function pathsFromFile($pp) {
+        $compiler = LlkTest::load(new Read($pp));
 
-		$i = 0;
-		$output = [];
-		foreach ($pathStrings as $strings) {
-			$output[] = ++$i . ': ' .  implode(' > ', $strings) .  PHP_EOL;
-		}
-		return $output;
-	}
+        $paths = $compiler->getRulePathsFromRoot();
+        $pathStrings = $compiler->getTokenPaths($paths);
 
-	public function testOneItem() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/oneItem.pp');
-		$this->string($output[0])->isEqualTo('1: Hello'.PHP_EOL);
-	}
+        $i = 0;
+        $output = [];
+        foreach ($pathStrings as $strings) {
+            $output[] = ++$i . ': ' . implode(' > ', $strings) . PHP_EOL;
+        }
+        return $output;
+    }
 
-	public function testTwoItem() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/concatenation.pp');
-		$this->string($output[0])->isEqualTo('1: Hello > world!'.PHP_EOL);
-	}
+    public function testOneItem() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/oneItem.pp');
+        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
+    }
 
-	public function testSubRule() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/subRule.pp');
-		$this->string($output[0])->isEqualTo('1: Hello'.PHP_EOL);
-	}
+    public function testTwoItem() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/concatenation.pp');
+        $this->string($output[0])->isEqualTo('1: Hello > world!' . PHP_EOL);
+    }
 
-	public function testChoice() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/choice.pp');
-		$this->string($output[0])->isEqualTo('1: Hello'.PHP_EOL);
-		$this->string($output[1])->isEqualTo('2: Goodbye'.PHP_EOL);
-	}
+    public function testSubRule() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/subRule.pp');
+        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
+    }
 
-	public function testRepetition() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/repetition.pp');
-		$this->string($output[0])->isEqualTo('1: Hello *'.PHP_EOL);
-	}
+    public function testChoice() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/choice.pp');
+        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: Goodbye' . PHP_EOL);
+    }
 
-	public function testWorld() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/world.pp');
-		$this->string($output[0])->isEqualTo('1: Hello > world!'.PHP_EOL);
-		$this->string($output[1])->isEqualTo('2: Goodbye > world!'.PHP_EOL);
-	}
+    public function testRepetition() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/repetition.pp');
+        $this->string($output[0])->isEqualTo('1: Hello *' . PHP_EOL);
+    }
 
-	public function testCircularReference1() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/circularReference1.pp');
-		$this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" *'.PHP_EOL);
-		$this->string($output[1])->isEqualTo('2: Hello'.PHP_EOL);
-	}
+    public function testWorld() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/world.pp');
+        $this->string($output[0])->isEqualTo('1: Hello > world!' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: Goodbye > world!' . PHP_EOL);
+    }
 
-	public function testCircularReference2() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/circularReference2.pp');
-		$this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" * > world!'.PHP_EOL);
-		$this->string($output[1])->isEqualTo('2: Goodbye > world!'.PHP_EOL);
-	}
+    public function testCircularReference1() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/circularReference1.pp');
+        $this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" *' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: Hello' . PHP_EOL);
+    }
 
-	public function testRuleRepetition() {
-		$output = $this->pathsFromFile(__DIR__.'/../pp/ruleRepetition.pp');
-		$this->string($output[0])->isEqualTo('1: Hello {2, 3}'.PHP_EOL);
-	}
+    public function testCircularReference2() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/circularReference2.pp');
+        $this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" * > world!' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: Goodbye > world!' . PHP_EOL);
+    }
+
+    public function testRuleRepetition() {
+        $output = $this->pathsFromFile(__DIR__ . '/../pp/ruleRepetition.pp');
+        $this->string($output[0])->isEqualTo('1: Hello {2, 3}' . PHP_EOL);
+    }
+
 }

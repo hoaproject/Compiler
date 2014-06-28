d24131907b19f82dff3644d34c2dc79e33e8cac7
flip111 <flip101@gmail.com>
2014-06-28 16:15:38 +0200

    Fixed unit tests

--- tests/units/Llk.php
+++ tests/units/Llk.php
@@ -28,56 +28,56 @@ class Llk extends atoum\test {
 
     public function testOneItem() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/oneItem.pp');
-        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello' . PHP_EOL);
     }
 
     public function testTwoItem() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/concatenation.pp');
-        $this->string($output[0])->isEqualTo('1: Hello > world!' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello > world' . PHP_EOL);
     }
 
     public function testSubRule() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/subRule.pp');
-        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello' . PHP_EOL);
     }
 
     public function testChoice() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/choice.pp');
-        $this->string($output[0])->isEqualTo('1: Hello' . PHP_EOL);
-        $this->string($output[1])->isEqualTo('2: Goodbye' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: goodbye' . PHP_EOL);
     }
 
     public function testRepetition() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/repetition.pp');
-        $this->string($output[0])->isEqualTo('1: Hello *' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello *' . PHP_EOL);
     }
 
     public function testWorld() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/world.pp');
-        $this->string($output[0])->isEqualTo('1: Hello > world!' . PHP_EOL);
-        $this->string($output[1])->isEqualTo('2: Goodbye > world!' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello > world' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: goodbye > world' . PHP_EOL);
     }
 
     public function testCircularReference1() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/circularReference1.pp');
-        $this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" *' . PHP_EOL);
-        $this->string($output[1])->isEqualTo('2: Hello' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: #CR:greeting#' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: hello' . PHP_EOL);
     }
 
     public function testCircularReference2() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/circularReference2.pp');
-        $this->string($output[0])->isEqualTo('1: * Circular Reference for index "greeting" * > world!' . PHP_EOL);
-        $this->string($output[1])->isEqualTo('2: Goodbye > world!' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: #CR:greeting# > world' . PHP_EOL);
+        $this->string($output[1])->isEqualTo('2: goodbye > world' . PHP_EOL);
     }
 
     public function testRuleRepetition() {
         $output = $this->pathsFromFile(__DIR__ . '/../pp/ruleRepetition.pp');
-        $this->string($output[0])->isEqualTo('1: Hello {2, 3}' . PHP_EOL);
+        $this->string($output[0])->isEqualTo('1: hello {2, 3}' . PHP_EOL);
     }
 
     public function testGroupRepetition() {
-        $output = $this->pathsFromFile(__DIR__.'/../pp/functionRepetition.pp');
-        $this->string($output[0])->isEqualTo('1: hello {2, 3}'.PHP_EOL);
+        $output = $this->pathsFromFile(__DIR__.'/../pp/groupRepetition.pp');
+        $this->string($output[0])->isEqualTo('1: ( hello > world ) {2, 2}'.PHP_EOL);
     }
 
     public function testGroupRepetitionAsNode() {
